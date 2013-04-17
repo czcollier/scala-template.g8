f24f40662bf65d7242fde37b7f4eb70c8f0c2808
Chris Collier <czcollier@gmail.com>
2013-04-17 17:20:53 -0400

    tweaks

--- src/main/g8/src/main/scala/$name__Camel$Main.scala
+++
@@ -1,3 +0,0 @@
-class Stub extends App {
-  println("I am working")
-}
---
+++ src/main/g8/src/main/scala/$package$/$name__Camel$Main.scala
@@ -0,0 +1,5 @@
+package $package$
+
+object $name;format="Camel"$Main extends App {
+  println("application $name is running");
+}
