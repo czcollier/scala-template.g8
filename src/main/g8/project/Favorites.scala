13b8743ff43cb66ac18209429da2cdf78165b770
Chris Collier <czcollier@gmail.com>
2013-06-12 17:19:03 -0400

    added favorites src file for libs

---
+++ src/main/g8/project/Favorites.scala
@@ -0,0 +1,99 @@
+import sbt._
+import Keys._
+import cc.spray.revolver.RevolverPlugin._
+
+object Build extends sbt.Build {
+  import Favorites._
+
+  lazy val myProject = Project("listserv", file("."))
+    .settings(Revolver.settings: _*)
+    .settings(
+      organization  := "net.xorf",
+      version       := "0.1.0",
+      scalaVersion  := "2.10.1",
+      scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
+      javaOptions   := Seq("-Xdebug", "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"),
+      resolvers     ++= Seq(
+        Repositories.typesafe,
+        Repositories.spray,
+        Repositories.scalaToolsSnaps //for salat and reactiveMongo 0.9-SNAPSHOT
+      ),
+      libraryDependencies ++= Seq(
+        Libraries.akkaActor,
+        Libraries.sprayCan,
+        Libraries.sprayRouting,
+        Libraries.sprayJson,
+        Libraries.sprayTest,
+        Libraries.reactiveMongo,
+
+        Libraries.salat,
+
+        Libraries.jodaTime,
+
+        Libraries.specs2,
+
+        Libraries.slf4j,
+        Libraries.logback
+      )
+    )
+}
+
+object Favorites {
+  object Repositories {
+    val typesafe        = "Typesafe repo"         at "http://repo.typesafe.com/typesafe/releases"
+    val scalaTools      = "Scala Tools Releases"  at "https://oss.sonatype.org/content/groups/scala-tools"
+    val scalaToolsSnaps = "Scala Tools Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+    val spray           = "spray repo"            at "http://repo.spray.io"
+    val novus           = "Novus Releases"        at "http://repo.novus.com/releases"
+  }
+
+  object Versions {
+    val akka          = "2.1.0"
+    val casbah        = "2.4.1"
+    val dispatch      = "0.9.2"
+    val jodaTime      = "1.6"
+    val lift          = "2.4"
+    val logback       = "1.0.0"
+    val reactiveMongo = "0.9"
+    val rogue         = "1.1.8"
+    val salat         = "1.9.2-SNAPSHOT"
+    val slf4j         = "1.6.4"
+    val specs2        = "1.14"
+    val spray         = "1.1-M7"
+    val sprayJson     = "1.2.3"
+    val squeryl       = "0.9.5-2"
+  }
+
+  object Libraries {
+    // akka 
+    val akkaActor       = "com.typesafe.akka"         %% "akka-actor"          % Versions.akka          % "compile"
+    val akkaSlf4j       = "com.typesafe.akka"         %  "akka-slf4j"          % Versions.akka
+
+    // spray
+    val sprayCan        = "io.spray"                  %  "spray-can"           % Versions.spray         % "compile"
+    val sprayRouting    = "io.spray"                  %  "spray-routing"       % Versions.spray         % "compile"
+    val sprayJson       = "io.spray"                  %% "spray-json"          % Versions.sprayJson     % "compile"
+    // MongoDB access
+    val casbah          = "org.mongodb"               %% "casbah"              % Versions.casbah        % "compile"
+    val liftMongo       = "net.liftweb"               %% "lift-mongodb"        % Versions.lift          % "compile"
+    val liftMongoRecord = "net.liftweb"               %% "lift-mongodb-record" % Versions.lift          % "compile"
+    val liftJson        = "net.liftweb"               %% "lift-json"           % Versions.lift          % "compile"
+    val rogue           = "com.foursquare"            %% "rogue"               % Versions.rogue         % "compile" intransitive()
+    val salat           = "com.novus"                 %% "salat"               % Versions.salat         % "compile"
+    val reactiveMongo   = "org.reactivemongo"         %% "reactivemongo"       % Versions.reactiveMongo % "compile"
+
+    // RDBMS access
+    val squeryl         = "org.squeryl"               %% "squeryl"             % Versions.squeryl       % "compile"
+
+    val dispatch        = "net.databinder.dispatch"   %% "dispatch-core"       % Versions.dispatch      % "compile"
+
+    // misc
+    val jodaTime        = "joda-time"                 %  "joda-time"           % Versions.jodaTime      % "compile"
+    val slf4j           = "org.slf4j"                 %  "slf4j-api"           % Versions.slf4j
+    val logback         = "ch.qos.logback"            %  "logback-classic"     % Versions.logback
+   
+    // testing
+    val specs2          = "org.specs2"                %% "specs2"              % Versions.specs2        % "test"
+    val sprayTest       = "io.spray"                  %  "spray-testkit"       % Versions.spray         % "test"
+  }
+}
